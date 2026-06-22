8a9340d6fb7872d187c3d220ea6170cfa8293485
516262300-star <516262300@qq.com>
2026-06-22 11:14:44 +0800

    Keep only no-console weekly report launcher

--- 启动拼多多周报生成器.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001 >nul
-cd /d "%~dp0"
-if exist ".venv\Scripts\pythonw.exe" (
-    start "" ".venv\Scripts\pythonw.exe" "weekly_report_app.pyw"
-) else (
-    start "" pythonw "weekly_report_app.pyw"
-)
